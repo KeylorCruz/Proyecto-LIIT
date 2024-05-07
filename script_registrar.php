723a27b0ca083765d2effdc3ea32ff962d46922e
JPRN01 <65576823+JPRN01@users.noreply.github.com>
2024-05-06 21:22:41 -0600

    script registrar
    php cli

---
+++ script_registrar.php
@@ -0,0 +1,28 @@
+<?php
+//Script hecho para ejecutar manualmente con PHP CLI
+$host = 'localhost';
+$dbname = 'ProjectLIIT';
+$username = 'root';
+$password = '4121';
+
+//Cambiar segun los datos del usuario que se desea agregar
+$user_id = 2020186460;
+$password = 'contraseña1';
+try {
+    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
+    $stmt = $pdo->prepare("INSERT INTO users (user_id, password) VALUES (?, ?)");
+    $stmt->execute([$user_id, $hashed_pass]);
+
+    if ($stmt->rowCount() > 0) {
+        echo "Usuario creado con éxito.";
+    } else {
+        echo "Error al crear usuario.";
+    }
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    $conn = null;
+}
+?>
